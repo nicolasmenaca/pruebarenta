0116c3d024850170ea1578873b623b89197d7348
Nicolas Meñaca <nicolasandresmenacatrujillo@gmail.com>
2024-04-17 10:51:32 -0500

    inicio de proyecto

---
+++ src/main/java/com/prueba/renta/pruebarenta/PruebarentaApplication.java
@@ -0,0 +1,13 @@
+package com.prueba.renta.pruebarenta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebarentaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebarentaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prueba/renta/pruebarenta/PruebarentaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prueba.renta.pruebarenta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebarentaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
