2eb8dd346786aebd4d837a86e82028c33e1c2faa
Nicolas Meñaca <nicolasandresmenacatrujillo@gmail.com>
2024-04-17 17:23:50 -0500

    entity propiedades

---
+++ src/main/java/com/prueba/renta/pruebarenta/entity/Propiedades.java
@@ -0,0 +1,37 @@
+package com.prueba.renta.pruebarenta.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "propiedades")
+public class Propiedades implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nombre;
+    private String ubicacion;
+    private Boolean disponibilidad;
+    private String url;
+    private BigDecimal precio;
+
+
+
+
+
+
+
+}
